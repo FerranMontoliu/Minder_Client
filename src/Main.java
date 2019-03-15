1878bb5ad07bcf366a160a8143cb523bf24c280d
annagm11 <anna.gutierrez@students.salle.url.edu>
2019-03-15 11:43:28 +0100

    Test Anna.

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,6 @@ public class Main {
     public static void main(String[] args) {
         ClientConfig cc = Json.parseJson();
         //Hola em dic Alba Massa.
+        //Hola em dic Anna.
     }
 }
