e636f8adb0751adbdd1fbd13470a6beb5e8f916e
Alba Massa <albamassasentis@gmail.com>
2019-03-18 08:45:35 +0100

    MenuWindow and MenuController added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,5 @@ public class Main {
         ClientConfig cc = Json.parseJson();
         LoginWindow loginWindow = new LoginWindow();
         loginWindow.setVisible(true);
-        //Pol soplapolles.
     }
 }
