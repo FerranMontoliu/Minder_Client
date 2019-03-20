91dee9f505a09c64f59be17ec4004992f4fe20ac
Alba Massa <albamassasentis@gmail.com>
2019-03-20 09:34:20 +0100

    ConnectPanel created. (per acabar el 20/03 a la nit)

---
+++ src/main/java/view/ConnectPanel.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * Panell que es mostrarà en cas que el mode Connect estigui seleccionat, on l'usuari podra acceptar o refusar usuaris
+ * en funcio de la seva imatge de perfil i informacio addicional a la que podra accedir prement el boto de mes informacio
+ */
+public class ConnectPanel extends JPanel {
+    public ConnectPanel(){
+
+
+    }
+
+}
