e15bd36e8816afbc7b81df5bc576ed04ca38b31e
Alba Massa <albamassasentis@gmail.com>
2019-04-20 10:45:34 +0200

    - No funciona el hashPassword, ja que es generen codings diferents per a una mateixa paraula, fet que provoca que el servidor no identifiqui l'usuari que vol iniciar sessio amb cap guardat a la base de dades. - Com que l'usuari no s'identifica amb cap, el (User)ObjectInput no es pot inicialitzar i per tant no es guarden les dades de l'usuari que ha iniciat sessio, nomes el nom i password (que venen del panell del login). PETA QUAN S'INTENTEN LLEGIR LES DADES DE L'USUARI (que estan a null si no s'inicialitzen) - La foto de text a base64 no he pogut comprovar si funciona - He actualitzat un camp de la BBDD (a part d'omplir-la) per a que l'edat sigui String, coincidint amb els atributs del User.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 import controller.LoginController;
-import network.ServerComunicationLogin;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import view.LoginWindow;
@@ -21,8 +20,46 @@ public class Main {
             w.registrarControlador(c);
             w.setVisible(true);
             //TODO ALBA: Comprovacio hash contrassenya mal funcionament
-            PasswordEncoder encoder = new BCryptPasswordEncoder();
-            System.out.println(encoder.encode("Minder19"));
+
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String existingPassword = passwordEncoder.encode("Minder19"); // Password entered by user
+            String dbPassword       =  "$2a$10$Rbmxa1Y2Z7eZ07qAcgt84edrIpBxULv6emOxcbQV7MjzMCDMRVYWq";// Load hashed DB password
+
+            if (passwordEncoder.matches(existingPassword, dbPassword)) {
+                // Encode new password and store it
+                System.out.println("equals");
+            } else {
+                System.out.println("error");
+            }
+
+            //altre metode que es pot/hauria de fer des del servidor
+
         });
     }
+    /**
+     * Check if the old password match the registered password
+     *
+     * @param id            id of the user with the password
+     * @param oldPassword   The password given to the system by a user
+     * @return              True if the password is incorrect, else it's false
+     * @throws Exception    Database errors
+     */
+    private void incorrectOldPassword(String id, String oldPassword) throws Exception {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        //User user = dao.getUserById(id);
+        //return !encoder.matches(oldPassword, user.getPassword());
+    }
+
+    /**
+     * Takes the id of a potential User and returns the User object. If user does not exist null
+     * is returned.
+     *
+     * @param id    ObjectId of the user in the database
+     * @return      A User object with matching id as argument
+     */
+    //public User getUserById(String id) {
+      //  MongoCollection collection = db.getClient().getCollection("users");
+        //return collection.findOne(new ObjectId(id)).as(User.class);
+    //}
+
 }
